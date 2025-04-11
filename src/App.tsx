05e7fa7a428b75e9f69f0a109b1fbccd77dff8f3
José <jcuencacoronado@gmail.com>
2025-04-11 17:23:27 -0400

    Renderizado condicional ? :

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
+/**
+ * truthy
+ * falsy
+ */
+
 function App() {
-  const list = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
+  const list: string[] = ["Elemento 1", "Elemento 2", "Elemento 3"];
   const handSelect = (elemento: string) => {
     console.log(elemento);
-  }
-  const handSelect2 = (elemento: string) => {
-    console.log("Mostrando: " + elemento);
-  }
+  };
+  const content = () => list.length !== 0 ? (
+      <List data={list} onSelect={handSelect} />
+    ) : (
+      <p>No hay elementos</p>
+    );
   return (
     <Card>
-      <CardBody title="Card title" text="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-      <List data={list} onSelect = {handSelect}/>
-      <List data={list} onSelect = {handSelect2}/>
+      <CardBody
+        title="Card title"
+        text="Some quick example text to build on the card title and make up the bulk of the card's content."
+      />
+      {content()}
     </Card>
   );
 }
 export default App;
-                                           
